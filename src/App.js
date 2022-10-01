1206fed865411e39b2a0eb6e364bff20ad2a5490
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-10-01 12:59:50 +0600

    milestone9 add

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+
+      <button type="button" className="btn btn-link">Link</button>
+
+      <br />
+      <div className="card-group">
+        <div className="card">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div className="card">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div className="card">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
